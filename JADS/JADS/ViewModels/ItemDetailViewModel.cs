2ae19a1d820cdbc073399b6011ffca0e1f6ca61b
Edwin Hendriks <hendriks.ed@gmail.com>
2020-04-15 09:43:39 +0200

    Add project files.

---
+++ JADS/JADS/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using JADS.Models;
+
+namespace JADS.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
